b8ed6cbc0f9fe53a7188555b3437d3bd6d2f173e
Lawlighty <49227867+Lawlighty@users.noreply.github.com>
2019-06-24 22:02:48 +0800

    Update reverseBits.java

--- 颠倒二进制位/reverseBits.java
+++ 颠倒二进制位/reverseBits.java
@@ -2,6 +2,9 @@ public class Solution{
   public int reverseBits(int n){
      int a=0;
       for(int i=0;i<=31;i++){
+        //n>>i 更新最后一位
+        // 1&(n>>i)若最后一位为1则保留最后一位,不然则为0
+        //31-i 当前项应该右移位数
           a=a+((1&(n>>i))<<(31-i));
       }
       return a;
