8f2075f78293ac769f017b876667647067e17392
Lawlighty <49227867+Lawlighty@users.noreply.github.com>
2019-06-24 21:39:15 +0800

    Update reverseBits.java

--- 颠倒二进制位/reverseBits.java
+++ 颠倒二进制位/reverseBits.java
@@ -1,12 +1,10 @@
 public class Solution{
   public int reverseBits(int n){
-    int res = 0;
-    for(int i=0;i<32;i++){
-      //当前项最后一位向左位移相应位数
-      res+=(n%2)<<(32-i);
-      //当前32二进制数右移一位
-      n = n/2;
-      //n = n>>1;
+     int a=0;
+        for(int i=0;i<=31;i++){
+            a=a+((1&(n>>i))<<(31-i));
+        }
+        return a;
     }
   }
 }
